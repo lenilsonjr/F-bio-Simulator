63c196e498360950d5ced6d9f6f667c1fa3f1964
Lenilson Júnior <lenilson@cubo9.com.br>
2015-12-10 01:27:33 -0300

    Commit inicial

---
+++ functions.php
@@ -0,0 +1,179 @@
+<?php
+/*
+    Fábio Simulator
+    Copyright LenilsonJr - falecom@lenilsonjr.com & Rannyeri Rodrigues - rannyerirbatista@gmail.com
+*/
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+header('Content-type: text/html; charset=utf-8');
+date_default_timezone_set("Brazil/East");
+
+/*
+    Função para exibir bordões de fábio
+*/
+function showSlogan() {
+
+    $slogans_f = fopen( "txt/slogans.txt", "r" );
+    $random = mt_rand( 0, count( file( "txt/slogans.txt" ) ) - 1 );
+
+    if ( $slogans_f ) {
+
+        $i = 0;
+        while ( $line = fgets( $slogans_f ) ) {
+
+            if ( $i == $random ) {
+
+                echo $line;
+
+            }
+
+            $i = $i + 1;
+
+        }
+
+    } else {
+
+        echo 'Ops, parece que o Pascalzim parou de funcionar...';
+
+    }
+
+}
+
+/*
+    Função para gerar exercicios
+*/
+function generateExercises( $num, $esp, $inst ) {
+
+    $exercises = [];
+
+    for ( $i = 1; $i <= $num; $i++ ) {
+
+        $imp_f  = fopen( "txt/imp.txt", "r" );
+
+        $str = 'Fazer um programa que ';
+
+        //Geramos o imperativo
+        $random = mt_rand( 0, count( file( "txt/imp.txt" ) ) - 1 );
+        $e = 0;
+        while ( $line = fgets( $imp_f ) ) {
+
+            if ( $e == $random ) {
+
+                $str = $str . $line . ' ';
+
+            }
+
+            $e = $e + 1;
+
+        }
+
+        //Geramos a especificação
+        $random = [];
+        for ( $u = 1; $u <= $esp; $u++ ) {
+
+            $esp_f  = fopen( "txt/esp.txt", "r" );
+
+            $random_g = mt_rand( 0, count( file( "txt/esp.txt" ) ) - 1 );
+
+            while ( in_array( $random_g, $random ) ) {
+
+                $random_g = mt_rand( 0, count( file( "txt/esp.txt" ) ) - 1 );
+
+            }
+
+            $random[] = $random_g;
+
+            $e = 0;
+            while ( $line = fgets( $esp_f ) ) {
+
+                //Aqui geramos um número aleátorio para substituir o %
+                $line = str_replace( '%', mt_rand( 2, 99 ), $line );
+
+                if ( $e == end( $random ) ) {
+
+                    if ( $u == $esp ) {
+
+                        $str = $str . ' e ' . $line;
+
+                    } elseif ( $u == 1 ) {
+
+                        $str = $str . $line;
+
+                    } else {
+
+                        $str = $str . ', ' . $line;
+
+                    }
+
+
+                }
+
+                $e = $e + 1;
+
+            }
+
+            fclose( $esp_f );
+
+        }
+
+        $str = $str . ' e ';
+
+        //Geramos a instrução
+        $random = [];
+        for ( $u = 1; $u <= $inst; $u++ ) {
+
+            $inst_f = fopen( "txt/inst.txt", "r" );
+
+            $random_g = mt_rand( 0, count( file( "txt/inst.txt" ) ) - 1 );
+
+            while ( in_array( $random_g, $random ) ) {
+
+                $random_g = mt_rand( 0, count( file( "txt/inst.txt" ) ) - 1 );
+
+            }
+
+            $random[] = $random_g;
+
+            $e = 0;
+            while ( $line = fgets( $inst_f ) ) {
+
+                if ( $e == end( $random ) ) {
+
+                    if ( $u == $inst ) {
+
+                        $str = $str . ' e ' . $line;
+
+                    } elseif ( $u == 1 ) {
+
+                        $str = $str . $line;
+
+                    } else {
+
+                        $str = $str .', ' . $line;
+
+                    }
+
+
+                }
+
+                $e = $e + 1;
+
+            }
+
+            fclose( $inst_f );
+
+        }
+
+        $str = $str . ".";
+        $str = str_replace( array("\n", "\r"), '', $str );
+
+        $exercises[] = $str;
+
+        fclose( $imp_f );
+
+    }
+
+    return $exercises;
+}
---
+++ index.php
@@ -0,0 +1,131 @@
+<?php
+/*
+    Fábio Simulator
+    Copyright LenilsonJr - falecom@lenilsonjr.com & Rannyeri Rodrigues - rannyerirbatista@gmail.com
+*/
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+require_once( 'functions.php' );
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>IFPE Fábio Simulator</title>
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css" media="screen" charset="utf-8">
+        <link rel="stylesheet" href="assets/css/style.css" media="screen" charset="utf-8">
+
+        <!-- SCRIPTS -->
+        <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+        <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+
+    </head>
+    <body>
+
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false">
+                        <span class="sr-only">Mudar navegação</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Fábio Simulator</a>
+                </div>
+
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#"><i><?php showSlogan(); ?></i></a></li>
+                </ul>
+
+            </div><!-- /.container-fluid -->
+        </nav>
+
+        <div class="container">
+
+            <div class="jumbotron">
+
+                <?php
+                if ( !isset ( $_REQUEST[ 'exercicios' ] ) ) {
+                ?>
+
+                <form id="formGerarProgramas" method="post">
+
+                    <div class="row">
+
+                        <div class="col-md-3">
+
+                            <div class="form-group">
+                                <label for="exercicios">Número de exercicios a gerar:</label>
+                                <input type="number" min="1" class="form-control" name="exercicios">
+                            </div>
+
+                        </div>
+                        <div class="col-md-3">
+
+                            <div class="form-group">
+                                <label for="especificações">Número de especificações a gerar:</label>
+                                <input type="number" min="1" max="7" class="form-control" name="especificacoes">
+                            </div>
+
+                        </div>
+                        <div class="col-md-3">
+
+                            <div class="form-group">
+                                <label for="instruções">Número de instruções a gerar:</label>
+                                <input type="number" min="1" max="5" class="form-control" name="instrucoes">
+                            </div>
+
+                        </div>
+                        <div class="col-md-3">
+
+                            <div class="form-group">
+                                <button style="margin-top: 14px;" type="submit" class="btn btn-lg center-block btn-success">GERAR!</button>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </form>
+
+                <?php
+                } else {
+
+                    $exercises = generateExercises( $_REQUEST['exercicios'], $_REQUEST['especificacoes'], $_REQUEST['instrucoes'] );
+
+                    foreach ( $exercises as $exercise ) {
+
+                        echo '<div class="row">';
+
+                        echo '<h3>'. $exercise .'</h3>';
+
+                        echo '</div>';
+                        echo '<hr>';
+
+                    }
+
+                ?>
+                <div class="row">
+
+                    <div class="pull-left">
+
+                        <a href="http://localhost" type="button" class="btn btn-lg btn-danger"> < VOLTAR</a>
+
+                    </div>
+
+                </div>
+                <?php
+                }
+                ?>
+
+            </div>
+
+        </div>
+
+    </body>
+</html>
